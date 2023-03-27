debe130edcda2902eb6e3659fbcf1a27db729f04
GG-va <127759678+GG-va@users.noreply.github.com>
2023-03-27 10:58:33 -0400

    Concatenated my HTML element to JS

--- index.js
+++ index.js
@@ -16,6 +16,35 @@ function formatDate(timestamp) {
 
 }
 
+function displayForecast() {
+    let forecastElement = document.querySelector("#forecast");
+
+    let forecastHTML = `<div class="row">`;
+
+    let days = ["Thu", "Fri", "Sat", "Sun"];
+
+days.forEach(function(day){
+    forecastHTML = forecastHTML + `
+                        <div class="col-2">
+                            <div class="weather-forecast-date">${day}
+                            </div>
+                            <img src="https://openweathermap.org/img/wn/04d@2x.png" alt="" width="42">
+                            <div class="weather-forecast-temperature">
+                                <span class="weather-forecast-temperature-max">
+                                    18°
+                                </span>
+
+                                <span class="weather-forecast-temperature-min">
+                                    12°
+                                </span>
+                        </div>
+                    </div> `;
+
+})
+
+    forecastHTML = forecastHTML + `</div>`;
+    forecastElement.innerHTML = forecastHTML;
+}
 
 function displayTemperature(response) {
     let temperatureElement = document.querySelector("#temperature");
@@ -84,3 +113,5 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 search("New York");
+
+displayForecast();
